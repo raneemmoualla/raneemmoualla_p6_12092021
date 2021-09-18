6b390057a2263bcfa86ed2af4540ac213f839f9a
raneem moualla <raneemmoualla@gmail.com>
2021-09-19 01:22:00 +0200

    creation fichier server.js

---
+++ backend/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const sauce = require('./models/sauce');
+const app = express();
+
+mongoose.connect('mongodb+srv://raneem-moualla:<Z.zeze1171981>@cluster0-pme76.mongodb.net/test?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+  app.use(bodyParser.json());
+module.exports = app;
